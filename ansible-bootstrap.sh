3ce943c318c073be9ccc4b78c0577fade8cd845d
JD White <jdeveloperw@gmail.com>
2014-02-02 09:18:09 -0800

    add -p to mkdir calls

--- ansible-bootstrap.sh
+++ ansible-bootstrap.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 
+sudo su
+cd /root/
 useradd ansible
 cp sudoers-ansible /etc/sudoers.d/ansible
 cp sshd_config /etc/ssh/sshd_config
 service ssh restart
-mkdir /home/ansible
+mkdir -p /home/ansible
 chown ansible /home/ansible
 chgrp ansible /home/ansible
 
-sudo -u ansible -H mkdir /home/ansible/.ssh
+sudo -u ansible -H mkdir -p /home/ansible/.ssh
 sudo -u ansible -H chmod 700 /home/ansible/.ssh
 sudo -u ansible -H cp ansible.pub /home/ansible/.ssh/authorized_keys
 sudo -u ansible -H chmod 644 /home/ansible/.ssh/authorized_keys
