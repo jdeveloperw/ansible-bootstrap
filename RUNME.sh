301da4a10b69cb35befef7f4dc5bc67daba10055
JD White <jdeveloperw@gmail.com>
2014-02-18 12:15:03 -0800

    Update scripts to detect and handle RHEL distributions

--- RUNME.sh
+++ RUNME.sh
@@ -1,9 +1,20 @@
 #!/bin/sh
 
-sudo su
-cd /root
-sudo apt-get install git
-git clone https://github.com/jdeveloperw/ansible-bootstrap.git
-mv ansible.pub ansible-bootstrap/
-cd ~/ansible-bootstrap/
-./ansible-bootstrap.sh
+if [[ `cat /proc/version | grep -i 'red hat'` ]]; then
+  DISTRIBUTION='RHEL'
+else
+  DISTRIBUTION=''
+fi
+
+if [ $DISTRIBUTION == 'RHEL' ]; then
+  PACKAGE_MANAGER="yum"
+else
+  PACAKGE_MANAGER="apt-get"
+fi
+
+sudo cd /root
+sudo $PACKAGE_MANAGER install git
+sudo git clone https://github.com/jdeveloperw/ansible-bootstrap.git
+sudo mv ansible.pub ansible-bootstrap/
+sudo cd ~/ansible-bootstrap/
+sudo ./ansible-bootstrap.sh
--- ansible-bootstrap.sh
+++ ansible-bootstrap.sh
@@ -1,14 +1,25 @@
 #!/bin/sh
 
-sudo su
-cd /root/
-useradd ansible
-cp sudoers-ansible /etc/sudoers.d/ansible
-cp sshd_config /etc/ssh/sshd_config
-service ssh restart
-mkdir -p /home/ansible
-chown ansible /home/ansible
-chgrp ansible /home/ansible
+if [[ `cat /proc/version | grep -i 'red hat'` ]]; then
+  DISTRIBUTION='RHEL'
+else
+  DISTRIBUTION=''
+fi
+
+if [ $DISTRIBUTION == 'RHEL' ]; then
+  SSHD_SERVICE="sshd"
+else
+  SSHD_SERVICE="ssh"
+fi
+
+sudo useradd ansible
+sudo mkdir -p /etc/sudoers.d
+sudo cp sudoers-ansible /etc/sudoers.d/ansible
+sudo cp sshd_config /etc/ssh/sshd_config
+sudo service $SSHD_SERVICE restart
+sudo mkdir -p /home/ansible
+sudo chown ansible /home/ansible
+sudo chgrp ansible /home/ansible
 
 sudo -u ansible -H mkdir -p /home/ansible/.ssh
 sudo -u ansible -H chmod 700 /home/ansible/.ssh
