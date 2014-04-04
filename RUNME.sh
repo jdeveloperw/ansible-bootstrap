fce0df619bf35f59eb2909f7d5fe9f5c5e264020
JD White <jdeveloperw@gmail.com>
2014-04-03 17:25:35 -0700

    Add missing $'s

--- RUNME.sh
+++ RUNME.sh
@@ -35,7 +35,7 @@ useradd ansible
 
 # Give ansible user permissions to run as root without a password prompt
 mkdir -p /etc/sudoers.d
-echo SUDOERS_ANSIBLE_CONTENT > /etc/sudoers.d/ansible
+echo $SUDOERS_ANSIBLE_CONTENT > /etc/sudoers.d/ansible
 
 # Enable public key authentication and disable password authentication
 # Remove any existing references to PasswordAuthentication
@@ -43,7 +43,7 @@ sed -i "s/.*PasswordAuthentication.*//g" $SSHD_CONFIG_FILE
 # Remove any existing references to PubkeyAuthentication
 sed -i "s/.*PubkeyAuthentication.*//g" $SSHD_CONFIG_FILE
 # Write the configuration we want to the sshd config file
-echo SSHD_CONFIG_CONTENT >> $SSHD_CONFIG_FILE
+echo $SSHD_CONFIG_CONTENT >> $SSHD_CONFIG_FILE
 service $SSHD_SERVICE restart
 
 # Create home directory for the ansible user
